084f284935bbe7cc9c42ea93c174716d90edc3f3
SunshineAtNoon <xuetingli@foxmail.com>
2017-04-15 09:47:31 +0800

    Create 476ReverseString.py

---
+++ 476ReverseString.py
@@ -0,0 +1,10 @@
+##############################
+#  Time Taken:59 ms／49.28%  #
+##############################
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return s[::-1]
